1e056f14315faa1413778d89ca53e9003035c57c
Artem Murashov <murashov.temka@yandex.ru>
2024-04-24 19:41:35 +0300

    first commit

---
+++ src/main/java/org/example/CashMachine.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CashMachine {
+
+    private Map<String, Map<Integer, Integer>> cash;
+
+    public CashMachine() {
+        this.cash = new HashMap<>();
+    }
+
+    public String execute(String command) {
+
+
+    }
+
+}
